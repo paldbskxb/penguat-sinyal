2d8819f12dd7ac252e8fa4d2b4710b134286cff7
paldbskxb <59151954+paldbskxb@users.noreply.github.com>
2019-12-26 13:23:55 +0800

    Add files via upload

---
+++ p.py
@@ -0,0 +1,16 @@
+print ('halo')
+Lol = input('masukan nama anda : ' )
+print ('selamat datang',Lol)
+pk = input('masukan password anda : ')
+L = input('apakah anda ingin menguatkan sinyal y/n :')
+if L == 'y':
+    print ('siap')
+else:
+    print ('oke gpp')
+import os
+
+os.system ("8.8.8.8")
+
+a=0
+while (a < 3):
+    print ('dah jalan')
